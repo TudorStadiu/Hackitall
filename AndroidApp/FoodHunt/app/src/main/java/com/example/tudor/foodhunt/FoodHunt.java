b7cefe494244268f909985b75f963ee4987425d0
CatalinTrand <catalin.lgg3@gmail.com>
2018-05-21 14:39:26 +0300

    Modified viewProducts activity; search + different stores; FoodHunt.java bug fix

--- AndroidApp/FoodHunt/app/src/main/java/com/example/tudor/foodhunt/FoodHunt.java
+++ AndroidApp/FoodHunt/app/src/main/java/com/example/tudor/foodhunt/FoodHunt.java
@@ -123,12 +123,15 @@ public class FoodHunt {
 
     private Vector<Product> produseKaufland;
     private Vector<Product> produseCarrefour;
+    private Vector<Product> toateProdusele;
 
     private Vector<MagazinMap> magazine;
 
     public FoodHunt() {
         produseKaufland = new Vector<>();
         produseCarrefour = new Vector<>();
+        toateProdusele = new Vector<>();
+
         magazine = new Vector<>();
 
         loadAllProductsFromServer();
@@ -202,6 +205,8 @@ public class FoodHunt {
         return produseCarrefour;
     }
 
+    Vector<Product> getToateProdusele(){ return  toateProdusele; }
+
     Vector<MagazinMap> getMagazine(){
         return magazine;
     }
@@ -213,6 +218,10 @@ public class FoodHunt {
             serverGetter.join();
             produseKaufland = serverGetter.produseKaufland.get();
             produseCarrefour = serverGetter.produseCarrefour.get();
+
+            toateProdusele.addAll(produseCarrefour);
+            toateProdusele.addAll(produseKaufland);
+
             magazine = serverGetter.magazine.get();
         } catch (Exception e) {
             e.printStackTrace();
--- AndroidApp/FoodHunt/app/src/main/java/com/example/tudor/foodhunt/viewProducts.java
+++ AndroidApp/FoodHunt/app/src/main/java/com/example/tudor/foodhunt/viewProducts.java
@@ -13,6 +13,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.SearchView;
 import android.widget.Spinner;
 import android.widget.TableLayout;
 import android.widget.TableRow;
@@ -22,17 +23,20 @@ import java.util.Vector;
 
 public class viewProducts extends AppCompatActivity {
 
+    String lastCategory;
+    String lastStore;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_products);
 
         final Button button = (Button) findViewById(R.id.button5);
-
         final TableLayout tableLayout = (TableLayout) findViewById(R.id.table);
         Spinner spinner = (Spinner) findViewById(R.id.spinner1);
-
+        Spinner stores = (Spinner) findViewById(R.id.spinner);
         final FoodHunt foodHunt = new FoodHunt();
+        final SearchView searchView = (SearchView)findViewById(R.id.searchView);
 
         Vector<String> categories = foodHunt.getCategories(foodHunt.getProduseKaufland());
         categories.add(0,"Toate");
@@ -40,17 +44,83 @@ public class viewProducts extends AppCompatActivity {
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this,R.layout.support_simple_spinner_dropdown_item,categories);
         spinner.setAdapter(adapter);
 
+        categories = new Vector<>();
+        categories.add("Toate");
+        categories.add("Kaufland");
+        categories.add("Carrefour");
+
+        adapter = new ArrayAdapter<>(this,R.layout.support_simple_spinner_dropdown_item,categories);
+        stores.setAdapter(adapter);
+
         showProducts(tableLayout,foodHunt.getProduseKaufland());
+
+        lastStore = "Toate";
+        lastCategory = "Toate";
+        lastCategory = "Toate";
 
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                lastCategory = parent.getItemAtPosition(position).toString();
                 showProducts(tableLayout,foodHunt.findAllCategory(parent.getItemAtPosition(position).toString(),foodHunt.getProduseKaufland()));
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
-                showProducts(tableLayout,foodHunt.getProduseKaufland());
+                lastCategory = "Toate";
+                switch (lastStore) {
+                    case "Toate":
+                        showProducts(tableLayout, foodHunt.getToateProdusele());
+                        break;
+                    case "Kaufland":
+                        showProducts(tableLayout, foodHunt.getProduseKaufland());
+                        break;
+                    case "Carrefour":
+                        showProducts(tableLayout, foodHunt.getProduseCarrefour());
+                        break;
+                }
+            }
+        });
+
+        stores.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                lastStore = parent.getItemAtPosition(position).toString();
+                switch (lastStore) {
+                    case "Toate":
+                        showProducts(tableLayout,foodHunt.findAllCategory(lastCategory,foodHunt.getToateProdusele()));
+                        break;
+                    case "Kaufland":
+                        showProducts(tableLayout,foodHunt.findAllCategory(lastCategory,foodHunt.getProduseKaufland()));
+                        break;
+                    case "Carrefour":
+                        showProducts(tableLayout,foodHunt.findAllCategory(lastCategory,foodHunt.getProduseCarrefour()));
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                lastStore = "Toate";
+                showProducts(tableLayout,foodHunt.findAllCategory(lastCategory,foodHunt.getProduseKaufland()));
+            }
+        });
+
+        searchView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                switch (lastStore) {
+                    case "Toate":
+                        showProducts(tableLayout,foodHunt.searchProduct(searchView.getQuery().toString(),foodHunt.findAllCategory(lastCategory,foodHunt.getToateProdusele())));
+                        break;
+                    case "Kaufland":
+                        showProducts(tableLayout,foodHunt.searchProduct(searchView.getQuery().toString(),foodHunt.findAllCategory(lastCategory,foodHunt.getProduseKaufland())));
+                        break;
+                    case "Carrefour":
+                        showProducts(tableLayout,foodHunt.searchProduct(searchView.getQuery().toString(),foodHunt.findAllCategory(lastCategory,foodHunt.getProduseCarrefour())));
+                        break;
+                }
             }
         });
 
